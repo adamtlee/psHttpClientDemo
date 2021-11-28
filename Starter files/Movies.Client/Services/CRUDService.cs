69441de7c33036098627a752b5c79231a82a78dd
adamtlee <adam.lee346@gmail.com>
2021-11-28 11:10:55 -0700

    500 error - Create Resource

--- Starter files/Movies.Client/Services/CRUDService.cs
+++ Starter files/Movies.Client/Services/CRUDService.cs
@@ -1,10 +1,13 @@
 ﻿using Movies.Client.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Movies.Client.Services
 {
@@ -16,10 +19,17 @@ namespace Movies.Client.Services
         {
             _httpClient.BaseAddress = new Uri("http://localhost:57863");
             _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+            // _httpClient.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/json"));
+            // _httpClient.DefaultRequestHeaders.Accept.Add(
+            //    new MediaTypeWithQualityHeaderValue("application/xml"));
         }
         public async Task Run()
         {
-            await GetResource();
+            // await GetResource(); 
+            // await GetResourceThroughHttpRequestMessage();
+             await CreateResource();
         }
 
         public async Task GetResource()
@@ -27,12 +37,74 @@ namespace Movies.Client.Services
             var response = await _httpClient.GetAsync("api/movies");
             response.EnsureSuccessStatusCode();
 
+            var movies = new List<Movie>();
             var content = await response.Content.ReadAsStringAsync();
-            var movies = JsonSerializer.Deserialize<IEnumerable<Movie>>(content,
-                new JsonSerializerOptions()
-                { 
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                }); 
+            if (response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                movies = JsonSerializer.Deserialize<List<Movie>>(content,
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+            }
+            else if (response.Content.Headers.ContentType.MediaType == "application/xml")
+            {
+                var serializer = new XmlSerializer(typeof(List<Movie>));
+                movies = (List<Movie>)serializer.Deserialize(new StringReader(content));
+            }
+            
+        }
+
+        public async Task GetResourceThroughHttpRequestMessage()
+        {
+            var request = new HttpRequestMessage(HttpMethod.Get, "api/movies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            var movies = new List<Movie>();
+            var content = await response.Content.ReadAsStringAsync();
+            if (response.Content.Headers.ContentType.MediaType == "application/json")
+            {
+                movies = JsonSerializer.Deserialize<List<Movie>>(content,
+                    new JsonSerializerOptions()
+                    {
+                        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                    });
+            }
+        }
+
+        public async Task CreateResource()
+        {
+            var movieToCreate = new MovieForCreation()
+            {
+                Title = "Gatsby",
+                Description = "The Great Gatsby yada yada yada",
+                DirectorId = Guid.Parse("aad2582e-8816-42c7-b4c7-c8e03784a17d"), 
+                ReleaseDate = new DateTimeOffset(new DateTime(1992, 9, 2)),
+                Genre = "Crime, Drama"
+            };
+
+            var serializedMovieToCreate = JsonSerializer.Serialize(movieToCreate);
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "api/movies");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            request.Content = new StringContent(serializedMovieToCreate);
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+            var response = await _httpClient.SendAsync(request);
+            response.EnsureSuccessStatusCode(); 
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            var createdMovie = JsonSerializer.Deserialize<Movie>(content,
+                 new JsonSerializerOptions()
+                 {
+                     PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                 });
+
         }
     }
 }
